5289e3564c8321d242131eb47f1b2dc60d2bfd88
Thiago <thiagonicomb.xd@gmail.com>
2020-09-17 17:20:00 -0300

    clase gaseosa

--- Mercado/kiosco/admin.py
+++ Mercado/kiosco/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from kiosco.models import *
 
 # Register your models here.
+
+
+admin.site.register(Gaseosa)
--- Mercado/kiosco/models.py
+++ Mercado/kiosco/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Gaseosa(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
